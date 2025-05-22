f5c527c737c97f70096ad3b613ec0222548892d3
xcs <evtjk@hotmail.com>
2025-05-22 12:10:54 +0800

    不使用硬件的tss功能进行task切换 原理还没搞明白 回过头需要复习

--- source/kernel/core/task.c
+++ source/kernel/core/task.c
@@ -32,42 +32,28 @@ static int tss_init (task_t * task, uint32_t entry, uint32_t esp) {
 }
 
 
-// static int tss_init(task_t* task,uint32_t entry,uint32_t esp){
 
-//     ASSERT(task != 0);
-
-//     int tss_sel = gdt_alloc_desc();
-//     if(tss_sel < 0){
-//         log_printf("wrong tss_slelector\n");
-//         return -1;
-//     }
-
-//     segment_desc_set(tss_sel,(uint32_t)&task->tss,sizeof(tss_t),
-//         SEG_P_PRESENT | SEG_DPL0 | SEG_TYPE_TSS
-//     );
-
-//     kernel_memset(&task,0,sizeof(task_t)); //
-//     task->tss.eip = entry;
-//     task->tss.esp = task->tss.esp0 = esp;
-//     task->tss.ss = task->tss.ss0 = KERNEL_SELECTOR_DS; 
-//     task->tss.es = task->tss.ds = task->tss.fs = task->tss.gs = KERNEL_SELECTOR_DS;
-//     task->tss.cs = KERNEL_SELECTOR_CS;
-//     task->tss.eflags = EFLAGS_DEFAULT | EFLAGS_IF;
-//     task->tss.iomap = 0;
-
-//     task->tss_sel = tss_sel;
-    
-//     return 0;
-// }
 
 
 int task_init(task_t* task,uint32_t entry,uint32_t esp){
     ASSERT(task!= 0);
     tss_init(task,entry,esp);
+    uint32_t* pesp = (uint32_t*) esp; 
+    if(pesp){
+        *(--pesp) = entry;
 
+        *(--pesp) = 0;
+        *(--pesp) = 0;
+        *(--pesp) = 0;
+        *(--pesp) = 0;
+        task->stack = pesp;
+    }
     return 0;
 }
 
+void simple_switch(uint32_t** from,uint32_t* to);
+
 void task_switch_from_to(task_t *form, task_t* to){
-    switch_to_tss(to->tss_sel); 
+    // switch_to_tss(to->tss_sel); 
+    simple_switch(&form->stack,to->stack);
 }
